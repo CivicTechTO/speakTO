2e65b554e9dcfd79fc2c0f6fb4e4bc468fced743
Devin Nathan-Turner <devin.nathan-turner@thinkresearch.com>
2016-09-17 15:14:39 -0400

    readded the print statement to recognizer. need to redirect in the bash call

--- transcribe.py
+++ transcribe.py
@@ -2,11 +2,10 @@ import speech_recognition as sr
 import sys
 
 AUDIO_FILE = str(sys.argv[1])
+WIT_API_KEY = "KGYGTEHJJZERAUVBPDQULMGBERGI5E4E"
 
 recognizer = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = recognizer.record(source)
 
-WIT_API_KEY = "KGYGTEHJJZERAUVBPDQULMGBERGI5E4E"
-
-return recognizer.recognize_wit(audio, key=WIT_API_KEY)
+print(recognizer.recognize_wit(audio, key=WIT_API_KEY))
