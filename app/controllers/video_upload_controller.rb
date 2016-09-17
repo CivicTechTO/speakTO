25e0981bfe100c4803f7bdf68a67ca3f30900b92
Vincent Li <vincent.li@thinkresearch.com>
2016-09-17 15:57:29 -0400

    Finished controller

--- app/controllers/video_upload_controller.rb
+++ app/controllers/video_upload_controller.rb
@@ -5,7 +5,10 @@ class VideoUploadController < ApplicationController
   end
 
   def upload
-  	debugger
+  	@file_name = params[:fname]
+  	@file_data = params[:data]
+  	@file_type = @file_data.content_type
+  	@file_object = @file_data.tempfile
   end
 
 end
