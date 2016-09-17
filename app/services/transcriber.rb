57c903c3988de0ddc8a4ef14213a183595bd9231
Sharon <sharon.peishan.kennedy@gmail.com>
2016-09-17 15:30:57 -0400

    speech to text

--- app/services/transcriber.rb
+++ app/services/transcriber.rb
@@ -4,13 +4,14 @@ class Transcriber
   def initialize(audio_filepath)
     @audio_filepath = audio_filepath
     @base_url = 'https://api.wit.ai/message'
-    @api_key = 'KGYGTEHJJZERAUVBPDQULMGBERGI5E4E'
+    @api_key = 'U7TTRVYRFZX7KEEQIDIZ3CFN5PUO4BLA'
   end
 
   def call
     response = HTTParty.post(
                              @base_url, 
-                             headers: { Authorization: "Bearer #{@api}"},
-                             query: { j})
+                             headers: { 'Authorization': "Bearer #{@api}"},
+                             query: { q: 'test'})
+    binding.pry
   end
 end
