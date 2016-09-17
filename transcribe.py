22377abbac93c33b9653a0ef63738ec0b3c63f86
Devin Nathan-Turner <devin.nathan-turner@thinkresearch.com>
2016-09-17 15:03:02 -0400

    Got rid of some of the try/excepts

--- transcribe.py
+++ transcribe.py
@@ -9,9 +9,4 @@ with sr.AudioFile(AUDIO_FILE) as source:
 
 WIT_API_KEY = "KGYGTEHJJZERAUVBPDQULMGBERGI5E4E"
 
-try:
-    return recognizer.recognize_wit(audio, key=WIT_API_KEY)
-except sr.UnknownValueError:
-    print("could not understand audio")
-except sr.RequestError as e:
-    print("could not request results from wit; {0}".format(e))
+return recognizer.recognize_wit(audio, key=WIT_API_KEY)
